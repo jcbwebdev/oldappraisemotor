01d86cc50b8d71d561dde100f09bc4a191d96b7d
JC Bongaitan <bongaitan.jc23@gmail.com>
2025-10-06 19:41:00 +0800

    updates old

--- inc/_config-cca-prod.php
+++ inc/_config-cca-prod.php
@@ -6,12 +6,12 @@
     define('DB_PASS','');
     define('DB_CHARSET','utf8');
 
-    define('PASSWORD_SALT','CHANGE_THIS_TO_RANDOM_STRING');
-    define('AES_ENCRYPTION_KEY', 'CHANGE_THIS_TO_RANDOM_STRING');
+    define('PASSWORD_SALT','oH@X-wVDua3XvEKRx4e7T_XB9*EJYPd!A7pfXxTB89ZrT!@vfvwkWFG9EN8LJzbgxm.yNKgY@yvp_-@zDraTz@hiApKuctyHT-@m*rYGpgMR6ok7KaLAoY9R6r7mUBXi3jD*A3hWq-d.sQ3RYm8__-4rz4op6.wwxHamB4ku.hh-!hmVtso**s7i8za6Nn*oADies6dBFPZs2DyXt6xnsKkWjW@CPHDVrmPLEBF.L@v4UYj42k3Q*n9b!d9xeknE*2x_tFXDicwhj_vMzGYWUGH!6on7mwZJAkq');
+    define('AES_ENCRYPTION_KEY', 'tY!CYV2!Y_z36qJHT6wnRyqb_bGWnc@QhV-b@o7MFdXq.__Q.c47yY-DPB-TKHYj_K3Qz-tss_fMFFjrW*HVE7EdP!X3F8-NzLt_r3Jyc8Q3mE6AuNRzh.njCLkxziUzQTxvnha2LpC7sRZCPPBP*hTxL9ZoMK*9X@G2V4m@3kqha3vwnWYj*UUdAPgz*-9Ktk*PZmqz3Wsai_Ms*QBjxJHKsA._ME9sHLKFVCqWX8U7ckJm4yF6WzDuJrqKYuiL4WTjU98gUogQ4ccKeNx487rzHupWFV24QqZ3Qm2tX8NrhksT!f7jtjyYsoacm_WKYMQzGVB*W72xAzp6bGXHfrGnEZ6TvtPHQr4P@HLBTaaiz-BMRz-dnh-itqkBoCkMBY.pkcakxRDe3yjupM3GT44YeRKkKGwtuUvzUDMfgD4bdQiTVLJKxH..zZ4L44@@-uc74Y2Y24rjnVFZ.XCu6gPWZiiLc6jNQKoEZruV!BMNhUijZ-GmkB3K8YZ4evAFjJm8rxXYcr@bXH3puE9rvLo2qecLAWvbV8fs3EqJBhZVjA2782qbXgee_pbA!mQiCaT_TM@VWEU@x-wrJheKhN6uMRkq*prM8MxtcF-XYQVZtKmxsPN@UwzBRa22*E*wRa3!7ZYTNg4pK_VBCrcm@avjc924*PH@jpUDAQ8bdUTzmHq6d-*YBsY@reDd3ru.y!NdoR6PQ*LMCzUwbj@gVbHqPCzGirfjC@XJnuKiqNm9z-9rF*xGZxf8UtxkdkFG_G4fvHqCDPwyVVHuc!3ZkLzPnJvKyYUWig!hQw7oYvBaAX*2HJhrLaqPxxjYTwpz3UJP4vtj9wz!.n7jT*68HkLE@B7MYdW8GKQjaFBAiwGHwmLnroz7PAimC3guiiQqodWqcK-AezX*ms.tbwzgQvUuGVGJ*dLA7EbeAxva!K6sC3timb-CWopTHDFZJZaRiEbLU.aueZvBqEELz.AhcZqdKA*mHm!X6F_xYHoNchhmURwu!BMoAaT7Y-RN9o3zjme4Yuhgps32rpZJFx2AMQ7*7bgW!!Z@LNEW4sCnkQsr.he6uGEuYrHv4ridM3bah7cVvf.PsqeUs.mp@ELoQw.ofjWQX6-8FU4sJrJwdM9bGRevo_gn7*-TiDbcVNE!k46W2B3pRK4KyBETfxw!2ZTUELaZYp.P.pQwwWmVd*DvXNKLPokQG2MYV*Lkqw.VMCc6NZGjze_T3BvgKjK!D2gkerfRwpPv*kxPFdxV*4Z3N4K2XEcRXKBXsYwDKjF*LrK*w!dfCz_*p7@MFUzE');
 
-    define('SITEFQDN', 'yourdomain.com');
-    define('DOCUMENT_ROOT', '/path/to/your/app');
-    define('SITENAME', 'Click Car Auction');
+    define('SITEFQDN', 'old.appraisemotor.co.uk');
+    define('DOCUMENT_ROOT', '/home/forge/oldappraisemotor-im2ljrdk.on-forge.com/current');
+    define('SITENAME', 'Old Appraise');
     define('SITEEMAIL', 'your-email@example.com');
     define('SITESENDEREMAIL', 'noreply@yourdomain.com');
 
