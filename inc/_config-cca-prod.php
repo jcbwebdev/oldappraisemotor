5e803e2bd395e4b3484322a388f3c3c8e7f257b3
JC Bongaitan <bongaitan.jc23@gmail.com>
2025-10-06 18:29:23 +0800

    updates

---
+++ inc/_config-cca-prod.php
@@ -0,0 +1,23 @@
+<?php
+
+    define('DB_HOST', 'localhost');
+    define('DB_NAME','oldclickcar');
+    define('DB_USER','root');
+    define('DB_PASS','');
+    define('DB_CHARSET','utf8');
+
+    define('PASSWORD_SALT','CHANGE_THIS_TO_RANDOM_STRING');
+    define('AES_ENCRYPTION_KEY', 'CHANGE_THIS_TO_RANDOM_STRING');
+
+    define('SITEFQDN', 'yourdomain.com');
+    define('DOCUMENT_ROOT', '/path/to/your/app');
+    define('SITENAME', 'Click Car Auction');
+    define('SITEEMAIL', 'your-email@example.com');
+    define('SITESENDEREMAIL', 'noreply@yourdomain.com');
+
+    define('USER_UPLOADS', '/user_uploads');
+    define('VEHICLE_IMAGES','/vehicle-images');
+    define('AUCTION_CONNECTION','//auction.yourdomain.com');
+    
+    define('CDL_USERNAME','YOUR_CDL_USERNAME');
+    define('CDL_PASSWORD','YOUR_CDL_PASSWORD');
